7806526825a37f3b29aa517fe2369464d8bc4aa5
ducviet2504 <ducviet2504@gmail.com>
2022-09-08 22:22:32 +0700

    update code

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-{/* 
+
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
@@ -38,7 +38,7 @@ export default function Home() {
           >
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a> */}
+          </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
